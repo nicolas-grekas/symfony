a81574b9e2b4d519ab26acea9ea8780756716399
cvilleger <villeger.c@gmail.com>
2018-04-06 19:04:20 +0200

    Fix invalid cookie error message

--- src/Symfony/Component/HttpFoundation/Tests/Fixtures/response-functional/invalid_cookie_name.php
+++ src/Symfony/Component/HttpFoundation/Tests/Fixtures/response-functional/invalid_cookie_name.php
@@ -7,6 +7,5 @@ $r = require __DIR__ . '/common.inc';
 try{
     $r->headers->setCookie(new Cookie('Hello + world', 'hodor'));
 } catch (\InvalidArgumentException $e){
-    return null;
+    echo $e->getMessage();
 }
-$r->sendHeaders();
