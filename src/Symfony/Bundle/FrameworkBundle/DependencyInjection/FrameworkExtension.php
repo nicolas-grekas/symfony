7bd5f190bef251e4f18d7e3b40bad57951713454
Alexander M. Turek <me@derrabus.de>
2019-08-23 22:10:22 +0200

    Fix tests for the DI component.

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -1992,9 +1992,7 @@ class FrameworkExtension extends Extension
     }
 
     /**
-     * Returns the base path for the XSD files.
-     *
-     * @return string The XSD base path
+     * {@inheritdoc}
      */
     public function getXsdValidationBasePath()
     {
