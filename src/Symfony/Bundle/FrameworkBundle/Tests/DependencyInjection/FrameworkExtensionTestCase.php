a9b7c79ccf198c6fe7ab124f3d9a2f320b84fb99
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-09-07 17:26:12 +0200

    [Mime][FrameworkBundle] Add MimeBundle
    (cherry picked from commit 548f54023f8d0cc5fee1e24b28a1939fa6482e64)
    (cherry picked from commit 461930dfd63e1a9557a5ec89312348d9fe9d2d5e)

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/FrameworkExtensionTestCase.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/FrameworkExtensionTestCase.php
@@ -82,6 +82,8 @@ use Symfony\Component\Lock\Store\FlockStore;
 use Symfony\Component\Lock\Store\SemaphoreStore;
 use Symfony\Component\Mailer\EventListener\InMemoryPgpPublicKeyRepository;
 use Symfony\Component\Mailer\EventListener\InMemorySmimeCertificateRepository;
+use Symfony\Component\Mailer\EventListener\PgpMimeEncryptedMessageListener;
+use Symfony\Component\Mailer\EventListener\PgpMimeSignedMessageListener;
 use Symfony\Component\Mailer\Header\TrackingHeader;
 use Symfony\Component\Messenger\Attribute\AsMessage;
 use Symfony\Component\Messenger\Bridge\AmazonSqs\Transport\AmazonSqsTransportFactory;
@@ -2994,8 +2996,8 @@ abstract class FrameworkExtensionTestCase extends TestCase
 
     public function testMailerPgp()
     {
-        if (!class_exists(PgpSigner::class)) {
-            $this->markTestSkipped('This test requires symfony/mime 8.2 or higher.');
+        if (!class_exists(PgpSigner::class) || !class_exists(PgpMimeSignedMessageListener::class)) {
+            $this->markTestSkipped('This test requires symfony/mime 8.2 and symfony/mailer 8.2 or higher.');
         }
 
         $container = $this->createContainerFromFile('mailer_with_pgp');
@@ -3027,8 +3029,8 @@ abstract class FrameworkExtensionTestCase extends TestCase
 
     public function testMailerPgpEncrypterFailsAndDoesNotEncryptForTheSenderByDefault()
     {
-        if (!class_exists(PgpEncrypter::class)) {
-            $this->markTestSkipped('This test requires symfony/mime 8.2 or higher.');
+        if (!class_exists(PgpEncrypter::class) || !class_exists(PgpMimeEncryptedMessageListener::class)) {
+            $this->markTestSkipped('This test requires symfony/mime 8.2 and symfony/mailer 8.2 or higher.');
         }
 
         $container = $this->createContainerFromFile('mailer_with_pgp_repository');
