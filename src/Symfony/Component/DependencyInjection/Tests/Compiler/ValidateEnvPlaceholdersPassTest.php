2125b9551ae03c0acc17a43245a2738ddce9f738
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-09-13 09:43:14 +0200

    [Config] Inline the env vars of the "enabled" node of enableable sections
    An extension decides what to register by reading $config['enabled'], so the
    value has to be known while the container is compiled. Until now it received a
    placeholder there, which is always truthy, so a section configured with an env
    var was enabled whatever the value of that var.
    
    The node generated by canBeEnabled() and canBeDisabled() now inlines the env
    vars it references. Compiling fails when the value is not known at that point,
    naming the option that needs it.

--- src/Symfony/Component/DependencyInjection/Tests/Compiler/ValidateEnvPlaceholdersPassTest.php
+++ src/Symfony/Component/DependencyInjection/Tests/Compiler/ValidateEnvPlaceholdersPassTest.php
@@ -370,6 +370,25 @@ class ValidateEnvPlaceholdersPassTest extends TestCase
         $this->doProcess($container);
     }
 
+    public function testTheEnabledNodeOfAnEnableableSectionInlinesItsEnvVar()
+    {
+        $_ENV['SECTION_ENABLED'] = 'false';
+
+        $container = new ContainerBuilder();
+        $container->registerExtension($ext = new EnvExtension(new EnableableConfiguration()));
+        $container->prependExtensionConfig('env_extension', [
+            'section' => ['enabled' => '%env(bool:SECTION_ENABLED)%'],
+        ]);
+
+        try {
+            $this->doProcess($container);
+        } finally {
+            unset($_ENV['SECTION_ENABLED']);
+        }
+
+        $this->assertFalse($ext->getConfig()['section']['enabled']);
+    }
+
     private function doProcess(ContainerBuilder $container): void
     {
         (new MergeExtensionConfigurationPass())->process($container);
@@ -449,6 +468,20 @@ class ConfigurationWithArrayNodeRequiringOneElement implements ConfigurationInte
                     ->requiresAtLeastOneElement()
                     ->scalarPrototype()->end()
                 ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
+
+class EnableableConfiguration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('env_extension');
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('section')->canBeEnabled()->end()
             ->end();
 
         return $treeBuilder;
